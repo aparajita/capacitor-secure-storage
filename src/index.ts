662c4aec43db6ad86f1a4fe4f8295bdf556d60b6
cosmin.cazac <cosmin.cazac@bitpanda.com>
2023-06-13 11:42:17 +0200

    removes console.log

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,5 @@
 import { registerPlugin } from '@capacitor/core'
 import type { SecureStoragePlugin } from './definitions'
-import info from './info.json'
-
-console.log(`loaded ${info.name} v${info.version}`)
 
 const proxy = registerPlugin<SecureStoragePlugin>('SecureStorage', {
   web: async () =>
