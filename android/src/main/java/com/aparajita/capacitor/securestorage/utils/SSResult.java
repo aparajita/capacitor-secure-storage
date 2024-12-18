1576f6ba168fe0db6a6b69b2c3cd68bc9432c2c5
DarkEdges <nirving.darkedges@darkedges.com>
2024-12-18 16:09:37 +1100

    feat(biometric): updating

--- android/src/main/java/com/aparajita/capacitor/securestorage/utils/SSResult.java
+++ android/src/main/java/com/aparajita/capacitor/securestorage/utils/SSResult.java
@@ -4,22 +4,22 @@ import com.aparajita.capacitor.securestorage.exceptions.SSSecurityError;
 
 public class SSResult<T> {
 
-    private SSSecurityError mError = null;
-    private T mResult = null;
+  private SSSecurityError mError = null;
+  private T mResult = null;
 
-    public SSResult(SSSecurityError mError) {
-        this.mError = mError;
-    }
+  public SSResult(SSSecurityError mError) {
+    this.mError = mError;
+  }
 
-    public SSResult(T result) {
-        mResult = result;
-    }
+  public SSResult(T result) {
+    mResult = result;
+  }
 
-    public SSSecurityError getError() {
-        return mError;
-    }
+  public SSSecurityError getError() {
+    return mError;
+  }
 
-    public T getResult() {
-        return mResult;
-    }
+  public T getResult() {
+    return mResult;
+  }
 }
