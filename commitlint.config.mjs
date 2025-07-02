8fd80fecc50ef065273a069dd3c6f0c3bfb3cd6f
Aparajita <aparajita@aparajita.com>
2025-07-01 22:39:03 -0700

    feat: update config files to ESM

--- commitlint.config.mjs
+++ commitlint.config.mjs
@@ -1,7 +1,9 @@
-module.exports = {
+const exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
     'body-max-line-length': [0, 'always', 0],
     'footer-max-line-length': [0, 'always', 0],
   },
 }
+
+export default exports
--- simple-git-hooks.mjs
+++ simple-git-hooks.mjs
@@ -3,7 +3,9 @@
 
   Be sure to run `simple-git-hooks` after modifying this file.
 */
-module.exports = {
+const exports = {
   'commit-msg': 'cat "$@" | "$PWD"/node_modules/.bin/commitlint',
   'pre-commit': 'pnpm lint',
 }
+
+export default exports
